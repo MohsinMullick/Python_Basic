e3ccee105c9aa2a5c8ed4a5260323356514b8788
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-09-07 12:43:26 +0600

    Add files via upload

---
+++ Type_Casting.py
@@ -0,0 +1,29 @@
+"""
+Casting:Changing a variable data type into another type
+like int to float
+str to int
+"""
+x=int(3)#x variable and value integer type
+y=str(3)#y variable declare and str data tpe value
+z=float(3)#z variable declare and float data type
+print(x)#python will always keep track of its current value so out put 3
+print(y)# python will always keep track of its current values so output string value 3
+print(z)#python will always keep track of its current value so output floating point 3.0
+
+
+x=5#variable x with integer value 5
+y="Mohsin"#variable y with string value Mohsin
+print(type(x))#python will  always keep track of its current value so output data type class int
+print(type(y))#python wil always keep track of its current value so output data type class str
+
+
+
+
+x=5#  # variable x with integer value 5
+y=float(x) # convert int to float (5 → 5.0)
+print(y)#python will always keep track of its current value
+
+
+x=3.7#variable x with floating vale 3.7
+y=int(x)#convert float to int(3.7→ 3)
+print(y)#python will always keep track of its current value so output integer type 3
