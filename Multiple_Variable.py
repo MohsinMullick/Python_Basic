e6320a62f92aa601b390b6c9367725c031f3f24d
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-09-07 13:04:38 +0600

    Add files via upload

---
+++ Multiple_Variable.py
@@ -0,0 +1,19 @@
+x,y,z="apple","orange","strawberry"
+print(x)
+print(y)
+print(z)
+
+
+x=y=z=["apple","banana","cherry"]
+print(x)
+print(y)
+print(z)
+
+
+
+#unpack a collection
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
