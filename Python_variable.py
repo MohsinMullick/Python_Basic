d4142fbe8c6bdb908fa55a6ebb2cd4faae8522d5
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-09-07 11:34:11 +0600

    Add files via upload

---
+++ Python_variable.py
@@ -0,0 +1,20 @@
+"""
+variable: variable are container for storing data values, you can change the value of a variable in your
+program at any time and python will always keep track of its current value.
+"""
+message="Hello, Python world!"#variable are containers for storing data values
+print(message)#python will always keep track of its current value
+
+message="Python in my life"#you can change the value of a variable in your program at any time
+print(message)#python will always keep track of its current value
+
+x=5#variable x and store value 5
+y="Mohsin"#variable y and store  value mohsin
+print(x)#python will always keep track of its current value
+print(y)#python will always keep track of its current value
+
+x=5#python store integer value 5
+x="Mohsin"#Python store value string type value mohsin
+print(x)
+"""print string value Because Python variables store only the latest assigned value, so
+ `x` becomes `"Mohsin"` and that’s what gets printed."""
